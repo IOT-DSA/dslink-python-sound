8407d0d73e937a2b732948219b7ddd16ed2a01e1
Logan Gorence <loganjohngorence@gmail.com>
2015-10-31 17:08:45 -0700

    Rewritten to use 0.3.1, and Python 2.

--- SoundLink.py
+++ SoundLink.py
@@ -2,6 +2,7 @@ import logging
 
 from dslink.DSLink import Configuration, DSLink
 from dslink.Node import Node
+from dslink.Profile import Profile
 import pyglet
 
 
@@ -9,43 +10,57 @@ class SoundLink(DSLink):
     def __init__(self):
         self.player = pyglet.media.Player()
         self.sound_logger = self.create_logger("Sound", logging.DEBUG)
-        super().__init__(Configuration("sound", responder=True))
+        DSLink.__init__(self, Configuration("sound", responder=True))
 
-        self.play_sound = Node("playSound", self.super_root)
-        self.play_sound.set_config("$name", "Play Sound")
-        self.play_sound.set_invokable("write")
-        self.play_sound.set_invoke_callback(self.play_sound_callback)
-        self.super_root.add_child(self.play_sound)
+        self.profile_manager.create_profile(Profile("playSound"))
+        self.profile_manager.register_callback("playSound", self.play_sound)
 
-        self.pause_sound = Node("pauseSound", self.super_root)
-        self.pause_sound.set_config("$name", "Pause Sound")
-        self.pause_sound.set_invokable("write")
-        self.pause_sound.set_invoke_callback(self.pause_sound_callback)
-        self.super_root.add_child(self.pause_sound)
+        self.profile_manager.create_profile(Profile("pauseSound"))
+        self.profile_manager.register_callback("pauseSound", self.pause_sound)
 
-        self.queue_sound = Node("queueSound", self.super_root)
-        self.queue_sound.set_config("$name", "Queue Sound")
-        self.queue_sound.set_invokable("write")
-        self.queue_sound.set_invoke_callback(self.queue_sound_callback)
-        self.queue_sound.set_parameters([
+        self.profile_manager.create_profile(Profile("queueSound"))
+        self.profile_manager.register_callback("queueSound", self.queue_sound)
+
+    def get_default_nodes(self):
+        root = self.get_root_node()
+
+        play_sound = Node("playSound", root)
+        play_sound.set_name("Play Sound")
+        play_sound.set_config("$is", "playSound")
+        play_sound.set_invokable("write")
+        root.add_child(play_sound)
+
+        pause_sound = Node("pauseSound", root)
+        pause_sound.set_name("Pause Sound")
+        pause_sound.set_config("$is", "pauseSound")
+        pause_sound.set_invokable("write")
+        root.add_child(pause_sound)
+
+        queue_sound = Node("queueSound", root)
+        queue_sound.set_name("Queue Sound")
+        queue_sound.set_config("$is", "queueSound")
+        queue_sound.set_invokable("write")
+        queue_sound.set_parameters([
             {
                 "name": "Sound Path",
                 "type": "string"
             }
         ])
-        self.super_root.add_child(self.queue_sound)
+        root.add_child(queue_sound)
 
-    def play_sound_callback(self, params):
+        return root
+
+    def play_sound(self, params):
         self.player.play()
 
-    def pause_sound_callback(self, params):
+    def pause_sound(self, params):
         self.player.pause()
 
-    def queue_sound_callback(self, params):
+    def queue_sound(self, obj):
         try:
-            self.player.queue(pyglet.media.load(params["Sound Path"]))
+            self.player.queue(pyglet.media.load(obj.params["Sound Path"]))
         except pyglet.media.avbin.AVbinException:
-            self.sound_logger.warning("Could not find file: %s" % params["Sound Path"])
+            self.sound_logger.warning("Could not find file: %s" % obj["Sound Path"])
 
 if __name__ == "__main__":
     SoundLink()
